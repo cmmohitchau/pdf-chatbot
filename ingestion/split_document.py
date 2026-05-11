d25f98d0cab40389022479f2a4980a2f22aaaf8c
cmmohitchau <cmmohitchau@gmail.com>
2026-05-11 09:06:10 +0530

    converted the text to Document and added bm25 retriever

--- ingestion/split_document.py
+++ ingestion/split_document.py
@@ -1,12 +1,22 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
+from langchain_core.documents import Document
+from langchain_core.bm25 import BM25Retriever
 def split_document(text):
     print("Parsing document...")
     try:
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         chunks = text_splitter.split_text(text)
-        print(f"Document parsed into {len(chunks)} chunks.")
-        return chunks
+
+        document = []
+        for chunk in chunks:
+
+            document.append(
+                Document(
+                    page_content=chunk,
+                    metadata={"source": "pdf_document"}
+                )
+            )        
+        return document
     except Exception as e:
         print(f"Error parsing document: {e}")
         return None
