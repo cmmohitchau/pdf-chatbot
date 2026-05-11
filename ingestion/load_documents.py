bbac7913f312c4a813926d967cc23f94515734e2
cmmohitchau <cmmohitchau@gmail.com>
2026-05-11 15:06:18 +0530

    added load document file

--- ingestion/load_documents.py
+++ ingestion/load_documents.py
@@ -1,13 +1,18 @@
-from PyPDF2 import PdfReader
+from pypdf import PdfReader
 
-def load_documents(file_path):
+def load_documents(file):
     print("Loading documents...")
     try:
-        reader = PdfReader(file_path)
+        reader = PdfReader(file.file)
+
         text = ""
+
         for page in reader.pages:
-            text += page.extract_text()
-        print("Document loaded successfully.")
+            extracted = page.extract_text()
+
+            if extracted:
+                text += extracted
+            
         return text
     except Exception as e:
         print(f"Error loading document: {e}")
