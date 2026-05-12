47924bc441e273fe5e571c3cf74ea4a80da5d4f2
cmmohitchau <cmmohitchau@gmail.com>
2026-05-12 15:52:30 +0530

    modified vector store

--- ingestion/vector_store.py
+++ ingestion/vector_store.py
@@ -20,7 +20,7 @@ embeddings = OpenAIEmbeddings(
 vectorstore = PineconeVectorStore(index=index, embedding=embeddings)
 bm25_retriever = None
 
-def vector_store(documents, source: str = "pdf_document") -> bool:
+def vector_store(documents , source) -> bool:
     global bm25_retriever
     print(f"Storing {len(documents)} chunks from '{source}'...")
 
@@ -28,10 +28,8 @@ def vector_store(documents, source: str = "pdf_document") -> bool:
         # Unique IDs per upload to avoid overwriting previous docs
         upload_id = uuid.uuid4().hex[:8]
         ids = [f"{upload_id}-chunk-{i}" for i in range(len(documents))]
-
         for doc in documents:
             doc.metadata["source"] = source
-
         vectorstore.add_documents(ids=ids, documents=documents)
 
         bm25_retriever = BM25Retriever.from_documents(documents)
