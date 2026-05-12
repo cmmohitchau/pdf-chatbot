e493930dbc7d66666e245e94e801854147a18939
cmmohitchau <cmmohitchau@gmail.com>
2026-05-12 15:52:08 +0530

    modified split document

--- ingestion/split_document.py
+++ ingestion/split_document.py
@@ -1,19 +1,26 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
-def split_document(text , source):
+def split_document(pages , source):
     try:
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        chunks = text_splitter.split_text(text)
+        
+        for page in pages:
+            print(page)
+            print("=" * 30 + "\n\n")
+        
 
         document = []
-        for chunk in chunks:
 
-            document.append(
-                Document(
-                    page_content=chunk,
-                    metadata={"source": source}
+        for page_num, page in enumerate(pages):
+            chunks = text_splitter.split_text(page)
+           
+            for chunk_id , chunk in enumerate(chunks):
+                document.append(
+                    Document(
+                        page_content=chunk,
+                        metadata={"source" : source , "page" : page_num + 1 , "chunk_id" : chunk_id + 1}
+                    )
                 )
-            )        
         return document
     except Exception as e:
         print(f"Error parsing document: {e}")
