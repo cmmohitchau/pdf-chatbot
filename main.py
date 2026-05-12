aec50f2277991a0db77899e18b3f794df31af51d
cmmohitchau <cmmohitchau@gmail.com>
2026-05-12 15:55:25 +0530

    added cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from ingestion.load_documents import load_documents
 from ingestion.split_document import split_document
@@ -10,6 +11,20 @@ from generation.generate import generate
 
 app = FastAPI()
 
+origins = [
+   "http://localhost:3000",
+   "https://myapp.com",
+]
+
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=origins, 
+allow_credentials=True, 
+allow_methods=["*"], 
+allow_headers=["*"], 
+)
+
 ALLOWED_CONTENT_TYPES = {"application/pdf"}
 
 @app.post("/upload")
@@ -18,6 +33,7 @@ async def upload(file: UploadFile):
         raise HTTPException(status_code=400, detail="Only PDF files are supported.")
 
     texts = load_documents(file)
+
     if texts is None:
         raise HTTPException(status_code=422, detail="Failed to extract text from PDF.")
 
@@ -25,7 +41,7 @@ async def upload(file: UploadFile):
     if not documents:
         raise HTTPException(status_code=422, detail="Failed to split document into chunks.")
 
-    success = vector_store(documents, source=file.filename)
+    success = vector_store(documents , source = file.filename)
     if not success:
         raise HTTPException(status_code=500, detail="Failed to store vectors.")
 
@@ -38,13 +54,21 @@ async def query(q: str):
 
         hyde_answer = hyde(q)
         search_results = hybrid_search(hyde_answer)
+        print("search_results")
+        print(search_results)
+        print("=" * 30)
         reranked_docs = rerank(search_results , q)
+        print("reranked_docs")
+        print(reranked_docs)
+        print("=" * 30)
         compressed = compress(reranked_docs , q)
-        final_answer = generate(q , compressed.content)
+        print("compressed")
+        print(compressed)
+        print("=" * 30)
+        final_answer = generate(q , compressed)
 
-        return {"answer" : final_answer.content}
+        answers = final_answer.split("\n\n")
+        return {"answer" : answers}
     except Exception as e:
-        return {
-            "error" : e
-        }
+        raise HTTPException(status_code=500 , detail=str(e))
 
