ff74683f9e577e68f76aed7f21e60cce3d7eea93
cmmohitchau <cmmohitchau@gmail.com>
2026-06-04 09:35:09 +0530

    added chunk id and chunk_index_in_page metadata

--- ingestion/split_document.py
+++ ingestion/split_document.py
@@ -1,6 +1,10 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
+
+chunk_id = 0
+
 def split_document(pages , source):
+    global chunk_id
     try:
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     
@@ -9,11 +13,17 @@ def split_document(pages , source):
         for page_num, page in enumerate(pages):
             chunks = text_splitter.split_text(page)
            
-            for chunk_id , chunk in enumerate(chunks):
+            for chunk_index_in_page , chunk in enumerate(chunks):
+                chunk_id += 1
                 document.append(
                     Document(
                         page_content=chunk,
-                        metadata={"source" : source , "page" : page_num + 1 , "chunk_id" : chunk_id + 1}
+                        metadata={
+                            "source" : source,
+                            "page" : page_num + 1 ,
+                            "chunk_id" : chunk_id,
+                            "chunk_index_in_page" : chunk_index_in_page + 1
+                            }
                     )
                 )
         return document
