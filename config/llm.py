fe13d34c22f745fd44be2138862ea601f562d06a
cmmohitchau <cmmohitchau@gmail.com>
2026-06-04 09:45:54 +0530

    lowered the temperature for preventing the hallucination

--- config/llm.py
+++ config/llm.py
@@ -2,4 +2,4 @@ from langchain_groq import ChatGroq
 from .config import GROQ_API_KEY
 
 
-llm  = ChatGroq(api_key=GROQ_API_KEY , model="llama-3.1-8b-instant", temperature=0.7)
+llm  = ChatGroq(api_key=GROQ_API_KEY , model="llama-3.1-8b-instant", temperature=0.1)
